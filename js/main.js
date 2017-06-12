2dc54aa6bb3c4a163f4aee90b9dbada7f559925a
Mone Rivas <mone.rivas@gmail.com>
2017-06-12 10:14:06 -0500

    $.ajax usando atajos shorthand methods

--- js/main.js
+++ js/main.js
@@ -3,24 +3,18 @@ cargarPersonajes();
 };
 
 var cargarPersonajes= function(){
-    $.ajax({
-        url:"http://swapi.co/api/people/", /*se puede pasa la url como primer parametro del metogo ajax*/
-        method: "GET",
-        dataTypr: "json",
-        success:function(response){            
-            var personajes= response.results; /*Ponemos results xq en esta API en particular esta la info ahi, pero no es asi siempre*/
-            $("#total").text(response.count);
-            mostrarTotalPersonajes(total);
-            mostrarPersonajes(personajes);
-            
-        },
-        error: function(error){
-            console.log("error", error);
-        }
+    var url= "http://swapi.co/api/people/";
+    $.get(url, function(response){
+        var personajes= response.results;
+        var total= response.count;
+        mostrarTotalPersonajes(total);
+        mostrarPersonajes(personajes);
     });
+
 };
 
-var mostrarTotalPersonajes= function(){
+var mostrarTotalPersonajes= function(total){
+    $("#total").text(total);
     
 }
 
